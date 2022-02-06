76cc28fe018e515b4ed6297dfa327d6051552b04
Amnah Aziz <51858008+imun9@users.noreply.github.com>
2022-02-06 04:56:32 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FormatListBulletedIcon /> Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
